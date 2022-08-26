c45f5f59a58a398905c46c77c34aee79b6b1a2ea
Kevin Harrington <kharrington@softroboticsinc.com>
2022-08-26 14:38:58 -0400

    ik mode

--- iktest.groovy
+++ iktest.groovy
@@ -1,1 +1,10 @@
 //Your code here
+
+MobileBase base=DeviceManager.getSpecificDevice( "Fanuc_Delta_DR",{ScriptingEngine.gitScriptRun(	"https://github.com/madhephaestus/Fanuc_Delta_DR-3iB.git", "Fanuc_Delta_DR-3iB.xml", null )})
+
+
+
+println "Now we will move just one leg"
+DHParameterKinematics mainLimb = base.getAllDHChains().get(0)
+
+return null
