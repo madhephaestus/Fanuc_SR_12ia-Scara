bd81e6119699c4fbad70455c07e8c69a596b514f
Kevin Harrington <kharrington@softroboticsinc.com>
2022-12-12 11:39:51 -0500

    update the z rod

--- stlfix.groovy
+++ stlfix.groovy
@@ -3,7 +3,7 @@ import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
 
 import eu.mihosoft.vrl.v3d.CSG
 String type = "sr-12ia"
-type = "ts2-100-vb"
+//type = "ts2-100-vb"
 def name = "stl/"+type+"/"+"l3.STL"
 File servoFile = ScriptingEngine.fileFromGit(
 	"https://github.com/madhephaestus/Fanuc_SR_12ia-Scara.git",
@@ -12,7 +12,7 @@ File servoFile = ScriptingEngine.fileFromGit(
 CSG servo  = Vitamins.get(servoFile)
 			.toXMin()
 			.toYMin()
-			.toZMin()
+			.toZMax()
 servo=servo
 			.movey(-servo.getTotalY()/2)
 			.movex(-servo.getTotalY()/2)
