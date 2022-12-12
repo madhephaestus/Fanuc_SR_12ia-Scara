33ea5e172a31cfa4cac39426d8b97d62d9d1f379
Kevin Harrington <kharrington@softroboticsinc.com>
2022-12-12 14:54:59 -0500

    staubli stl

--- stlfix.groovy
+++ stlfix.groovy
@@ -4,7 +4,7 @@ import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
 import eu.mihosoft.vrl.v3d.CSG
 String type = "sr-12ia"
 type = "ts2-100-vb"
-def name = "stl/"+type+"/"+"l3.STL"
+def name = "stl/"+type+"/"+"l1.STL"
 File servoFile = ScriptingEngine.fileFromGit(
 	"https://github.com/madhephaestus/Fanuc_SR_12ia-Scara.git",
 	name);
@@ -12,10 +12,10 @@ File servoFile = ScriptingEngine.fileFromGit(
 CSG servo  = Vitamins.get(servoFile)
 			.toXMin()
 			.toYMin()
-			.toZMax()
+			.toZMin()
 servo=servo
 			.movey(-servo.getTotalY()/2)
-			.movex(-servo.getTotalY()/2)
+			.movex(-servo.getTotalY()/2-31)
 String filename =servoFile.getAbsolutePath()
 println filename
 FileUtil.write(Paths.get(filename),
